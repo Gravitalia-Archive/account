260591b0d5c8014bd2282bd31f8c49aa2fac9bf3
Hinome <57831472+RealHinome@users.noreply.github.com>
2022-11-01 16:19:43 +0100

    First commit 🎉

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from 'astro/config';
+import vue from "@astrojs/vue";
+import tailwind from "@astrojs/tailwind";
+
+import prefetch from "@astrojs/prefetch";
+import sitemap from "@astrojs/sitemap";
+import cloudflare from "@astrojs/cloudflare";
+
+// https://astro.build/config
+export default defineConfig({
+  output: "server",
+  integrations: [vue(), tailwind(), prefetch(), sitemap()],
+  adapter: cloudflare(),
+  site: "https://account.gravitalia.com"
+});
