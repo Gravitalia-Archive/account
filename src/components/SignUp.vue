d827221aafb651f403ac5647c58db4aa98ec13ac
Hinome <57831472+RealHinome@users.noreply.github.com>
2022-11-13 19:15:01 +0100

    Adding Sign up component ✨

---
+++ src/components/SignUp.vue
@@ -0,0 +1,20 @@
+<template>
+    
+</template>
+
+<script lang="ts">
+    export default {
+        data() {
+            return {
+                token: ""
+            }
+        },
+        mounted() {
+            document.onkeydown = (e) => {
+                if(e.code === "Enter") return; //this.next();
+            };
+        },
+        methods: {
+        }
+    }
+</script>
