eaaac2ea3f6754ff639a3ff157e5b0a18c49b9cf
Hinome <57831472+RealHinome@users.noreply.github.com>
2023-05-05 21:29:55 +0200

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue";
 import tailwind from "@astrojs/tailwind";
-
 import prefetch from "@astrojs/prefetch";
 import sitemap from "@astrojs/sitemap";
 import cloudflare from "@astrojs/cloudflare";
 
+// https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [vue(), tailwind(), prefetch(), sitemap()],
