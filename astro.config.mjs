61378ebd9d3851af664a1e584a754f50e233aff2
Hinome <57831472+RealHinome@users.noreply.github.com>
2023-05-05 22:13:04 +0200

    Rallback config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from 'astro/config';
-import cloudflare from "@astrojs/cloudflare";
-import prefetch from "@astrojs/prefetch";
 import tailwind from "@astrojs/tailwind";
+import prefetch from "@astrojs/prefetch";
 import sitemap from "@astrojs/sitemap";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://account.gravitalia.com/",
   output: "server",
+  integrations: [tailwind(), prefetch(), sitemap()],
   adapter: cloudflare(),
-  integrations: [prefetch(), tailwind(), sitemap()]
+  site: "https://account.gravitalia.com"
 });
