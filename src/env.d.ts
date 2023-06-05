359c0e5b41e712296782493a2d07f97c958330b2
Hinome <57831472+RealHinome@users.noreply.github.com>
2023-06-05 14:02:29 +0200

    Update env variables

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,8 @@
-/// <reference types="astro/client" />
+interface ImportMetaEnv {
+    readonly API_URL: string;
+    readonly CDN_URL: string;
+}
+  
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
